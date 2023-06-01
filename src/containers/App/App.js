3eb94de74a69e7b63f5c28373c93ed1fdbcae2e3
l-errony <99561455+l-errony@users.noreply.github.com>
2023-06-01 22:00:24 +0300

    Update App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -15,7 +15,7 @@ function App() {
     return (
         <Routes >
             <Route path={'/'} element={<MainLayout/>}>
-                <Route path='' element={<Home/>}/>
+                <Route path={'/'} element={<Home/>}/>
                 <Route path='ordering' element={<ShoppingCart/>}/>
             </Route>
         </Routes>
